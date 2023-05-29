2c499cb011379ae170ef53dc644773b70953cdc2
didgjstn585 <com6474@gmail.com>
2023-05-29 21:36:31 +0900

    Initial commit

---
+++ src/main/java/com/example/jpaquerydsl/JpaquerydslApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpaquerydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaquerydslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaquerydslApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jpaquerydsl/JpaquerydslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpaquerydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaquerydslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
